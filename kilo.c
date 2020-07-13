a14067b3383d5e42d5df1f418fdefc436ccd3d2e
Gun9niR <guozhidong12@126.com>
2020-07-13 15:55:16 +0800

    Add tildes to the left of each column

--- kilo.c
+++ kilo.c
@@ -3,37 +3,48 @@
 #include<errno.h>
 #include<stdio.h>
 #include<stdlib.h>
+#include<sys/ioctl.h>
 #include<termios.h>
 #include<unistd.h>
 
+/*** defines ***/
+
+#define CTRL_KEY(k) ((k) & 0x1f)  //a bit mask that sets the upper 3 bits of the character to 0，which is exactly how CTRL works
+
 /*** data ***/
 
-struct termios orig_terminos;
+struct editor_config {
+    int screenrows;
+    int screencols;
+    struct termios orig_terminos;
+};
+
+struct editor_config E;
 
 /*** terminal ***/
 
 void die(const char *s) {
-    /*
-    perror() looks at the global errno variable and prints a descriptive error message for it. 
-    t also prints the string given to it before it prints the error message, 
-    which is meant to provide context about what part of your code caused the error.
-    */
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    write(STDOUT_FILENO, "\x1b[H", 3);
+    //perror() looks at the global errno variable and prints a descriptive error message for it. 
+    //It also prints the string given to it before it prints the error message, 
+    //which is meant to provide context about what part of your code caused the error.
     perror(s);
     exit(1);
 }
 
 void disable_raw_mode()
 {
-    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_terminos) == -1)
+    if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &E.orig_terminos) == -1)
         die("tcsetattr");
 }
 
-void enable_raw_mode() 
+void enable_raw_mode()
 {
-    if(tcgetattr(STDIN_FILENO, &orig_terminos) == -1) die("tcgetattr");
+    if(tcgetattr(STDIN_FILENO, &E.orig_terminos) == -1) die("tcgetattr");
     atexit(disable_raw_mode); //register the function to be called when program exits
 
-    struct termios raw = orig_terminos;
+    struct termios raw = E.orig_terminos;
     //switch off a couple flags by making the bit on the corresponding position 0
     //iflag, oflag, cflag, lflag are unsigned int
     //BRKINT: switch off break condition which terminates the program
@@ -65,20 +76,115 @@ void enable_raw_mode()
     if(tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw) ==-1) die("tcsetattr");
 }
 
+char editor_read_key() 
+{
+    int nread;
+    char c;
+    while((nread = read(STDIN_FILENO, &c, 1)) != 1) {
+        if(nread == -1 && errno != EAGAIN) die("read");
+    }
+    return c;
+}
+
+int get_cursor_position(int *rows, int *cols)
+{
+    char buf[32];
+    unsigned int i=0;
+    //"6n" inquires the cursor position
+    if(write(STDOUT_FILENO, "\x1b[6n", 4) != 4) return -1;
+
+    while(i < sizeof(buf) -1) {
+        if(read(STDIN_FILENO, &buf[i], 1) != 1) break;
+        if(buf[i] == 'R') break;
+        i++;
+    }
+    buf[i]='\0';
+
+    if(buf[0] != '\x1b' || buf[1] != '[') return -1;
+    if(sscanf(&buf[2], "%d;%d", rows, cols) != 2) return -1;
+
+    return 0;
+}
+
+int get_window_size(int *rows, int *cols)
+{
+    struct winsize ws;
+
+    //'C' moves the cursor to the right
+    //'B' moves the cursor down. Both commands check the bound
+    if(ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        if(write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) !=12) return -1;
+        editor_read_key();
+        return get_cursor_position(rows, cols);
+    } else {
+        *cols = ws.ws_col;
+        *rows = ws.ws_row;
+        return 0;
+    }
+}
+
+/*** output ***/
+
+void editor_draw_rows()
+{
+    int y;
+    for(y = 0; y < E.screenrows; y++) {
+        write(STDOUT_FILENO, "~", 1);
+
+        if(y < E.screenrows - 1) {
+            write(STDOUT_FILENO, "\r\n", 2);
+        }
+    }
+}
+
+void editor_refresh_screen()
+{
+    //"x1b" is escape, "\x1b[2J" is an escape sequance
+    //Escape sequences always start with an escape character (27) followed by a [ character. 
+    //Escape sequences instruct the terminal to do various text formatting tasks, such as coloring text, moving the cursor around, and clearing parts of the screen.
+    //'J' clears the screen
+    //'0' clears the screen from the cursor up to the end
+    //'1' clears the scrren up to the cursor
+    //'2' clears the entire screen
+    write(STDOUT_FILENO, "\x1b[2J", 4);
+    //'H' positions the cursor. It takes to arguments, specifying the line and column, say "/x1b[1;1H"
+    //Line and column number start at 1
+    write(STDOUT_FILENO, "\x1b[H", 3);
+
+    editor_draw_rows();
+
+    write(STDOUT_FILENO, "\x1b[H", 3);
+}
+
+/*** input ***/
+
+void editor_process_keypress() 
+{
+    char c = editor_read_key();
+
+    switch(c) {
+        case CTRL_KEY('q'):
+        write(STDOUT_FILENO, "\x1b[2J", 4);
+        write(STDOUT_FILENO, "\x1b[H", 3);
+        exit(0);
+        break;
+    }
+}
+
 /*** init ***/
+void initEditor() 
+{
+    if(get_window_size(&E.screenrows, &E.screencols) == -1) die("get_window_size");
+}
 
 int main() {
     enable_raw_mode();
+    initEditor();
 
     while(1) {
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) die("read");
-        if(iscntrl(c))
-            printf("%d\r\n",c);
-        else 
-            printf("%d ('%c')\r\n", c, c);
-        if(c == 'q') break;
+        editor_refresh_screen();
+        editor_process_keypress();
     }
-
+    
     return 0;
 }
